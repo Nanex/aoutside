959fa4d21cbf5758ccff52be1b0a8ffe92c38f5e
Alexis Le Boulc'h <alexisleboulch@gmail.com>
2017-08-30 17:25:04 +0200

    up

---
+++ js/Aoutside8.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function(){
+    $("#front").click(function(){
+        $("#front").animate({
+            bottom: '150px',
+            height: '150px',
+            width: '150px'
+        });
+    });
+});*/
